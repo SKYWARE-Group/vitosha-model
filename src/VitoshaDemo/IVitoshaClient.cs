9b09bc37a67da8faf72e295e8e881b03f7fc5e70
Mikhail Kalatchev <kalatchev@gmail.com>
2025-05-15 18:56:24 +0300

    x

---
+++ src/VitoshaDemo/IVitoshaClient.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+
+namespace Skyware.Vitosha.Model.Demo;
+
+public interface IVitoshaClient
+{
+
+    [Post("/api/messages")]
+    Task<MessageResponse> SendMessage(Message message, [Header("x-api-key")] string apiKey);
+
+    [Get("/api/messages/{id}")]
+    Task<MessageResponse> GetMessageStatus(int id, [Header("x-api-key")] string apiKey);
+
+}
---
+++ src/VitoshaDtoModel/MessageResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Skyware.Vitosha.Model;
+
+/// <summary>
+/// Represents a response to message sending.
+/// </summary>
+public class MessageResponse
+{
+
+    /// <summary>
+    /// Status code.
+    /// </summary>
+    public int StatusCode { get; set; }
+
+    /// <summary>
+    /// Status message.
+    /// </summary>
+    public string Message { get; set; }
+
+    public Message MessageObj { get; set; }
+
+    public int? MessageId { get; set; }
+
+    public string Details { get; set; }
+
+}
