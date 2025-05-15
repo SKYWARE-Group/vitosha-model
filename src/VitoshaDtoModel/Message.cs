6c6980c0e41b293a8d760f4046495e69a7bfd050
Mikhail Kalatchev <kalatchev@gmail.com>
2025-05-15 15:05:35 +0300

    Initial commit

---
+++ src/VitoshaDtoModel/Message.cs
@@ -0,0 +1,54 @@
+﻿namespace Skyware.Vitosha.Model;
+
+/// <summary>
+/// Represents a message to be sent via Vitosha SMS service.
+/// </summary>
+public class Message
+{
+
+    /// <summary>
+    /// Is the message intended for viber
+    /// </summary>
+    public bool IsViber { get; set; } = false;
+
+    /// <summary>
+    /// Receiver's MSISDN
+    /// </summary>
+    public string Recipient { get; set; }
+
+    /// <summary>
+    /// Text of the SMS Message.
+    /// </summary>
+    public string Text { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Viber Button Text 
+    /// </summary>
+    public string ButtonName { get; set; }
+
+    /// <summary>
+    /// Viber Button URL
+    /// </summary>
+    public string ButtonUrl { get; set; }
+
+    /// <summary>
+    /// Viber Image URL
+    /// </summary>
+    public string ImageUrl { get; set; }
+
+    /// <summary>
+    /// Reference Id
+    /// </summary>
+    public string RefId { get; set; }
+
+    /// <summary>
+    /// Reference Id
+    /// </summary>
+    public string Note { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public MessageStatus[] Statuses { get; set; }
+
+}
---
+++ src/VitoshaDtoModel/SkywareResponse.cs
@@ -0,0 +1,45 @@
+﻿namespace Skyware.Vitosha.Model;
+
+public class SkywareResponse
+{
+
+    public SkywareResponse() { }
+
+    public SkywareResponse(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message;
+    }
+
+    public SkywareResponse(int statusCode, string message, int messageId)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        MessageId = messageId;
+    }
+
+    public SkywareResponse(int statusCode, string message, string details)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+
+    public SkywareResponse(int statusCode, string message, Message messageObj)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        MessageObj = messageObj;
+    }
+
+    public int StatusCode { get; set; }
+
+    public string Message { get; set; }
+
+    public Message MessageObj { get; set; }
+
+    public int? MessageId { get; set; }
+
+    public string Details { get; set; }
+
+}
